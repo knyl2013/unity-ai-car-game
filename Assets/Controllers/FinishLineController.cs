b59e9539a67ccf39027baa3358d5878c34769d44
LiWaiYip <1155115790@link.cuhk.edu.hk>
2021-04-20 11:47:26 +0800

    Init & Created Finish Line Controller

---
+++ Assets/Controllers/FinishLineController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLineController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name + " finishes at " + elapsedTime + " seconds");
+    }
+
+    private float elapsedTime;
+}
